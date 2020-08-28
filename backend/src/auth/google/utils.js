79f0405208d643194ef26c2f1dcf5bb865560744
Ciprian Andrei <andrei.stefanel.ciprian@gmail.com>
2020-08-28 13:19:20 +0300

    implemented updateProviderTokens middleware

--- backend/src/auth/google/utils.js
+++ backend/src/auth/google/utils.js
@@ -3,28 +3,42 @@ const jwt = require("jsonwebtoken");
 const CONFIG = require("../../config");
 const { updateUserByEmail } = require("../../db/interaction");
 
-module.exports = { updateProviderAccessToken, getUserProfile, extractUserProfile };
+module.exports = { updateProviderTokens, getUserProfile, extractUserProfile };
 
-async function updateProviderAccessToken(userEmail, providerRefreshToken) {
+// middleware
+async function updateProviderTokens(req, res, next) {
   // const GOOGLE_OAUTH2_TOKEN_URL = "https://oauth2.googleapis.com/token";
   const GOOGLE_OAUTH2_TOKEN_URL = "https://www.googleapis.com/oauth2/v4/token";
-  const body = {
-    client_id: CONFIG.GOOGLE_CLIENT_ID,
-    client_secret: CONFIG.GOOGLE_CLIENT_SECRET,
-    refresh_token: providerRefreshToken,
-    grant_type: "refresh_token",
-  };
-  const response = await axios({
-    method: "POST",
-    url: GOOGLE_OAUTH2_TOKEN_URL,
-    data: body,
-    headers: { "Content-Type": "application/json" },
-  });
+  // check if id_token expired:
+  const { exp } = jwt.decode(req.user.providerIdToken);
+  if (exp * 1000 < new Date().getTime()) {
+    console.log("ID TOKEN EXPIRED");
+    // create http request request
+    const body = {
+      client_id: CONFIG.GOOGLE_CLIENT_ID,
+      client_secret: CONFIG.GOOGLE_CLIENT_SECRET,
+      refresh_token: req.user.providerRefreshToken,
+      grant_type: "refresh_token",
+    };
+    const response = await axios({
+      method: "POST",
+      url: GOOGLE_OAUTH2_TOKEN_URL,
+      data: body,
+      headers: { "Content-Type": "application/json" },
+    });
 
-  // update accessToken in DB
-  const providerAccessToken = response.data.access_token;
-  updateUserByEmail({ providerAccessToken }, userEmail);
-  return providerAccessToken;
+    // console.log("response.data:", response.data);
+    // extract tokens from response
+    const providerAccessToken = response.data.access_token;
+    const providerIdToken = response.data.id_token;
+
+    // update req.user
+    req.user.providerAccessToken = providerAccessToken;
+    req.user.providerIdToken = providerIdToken;
+    // update accessToken in DB
+    updateUserByEmail({ providerAccessToken, providerIdToken }, req.user.email);
+  }
+  next();
 }
 
 async function getUserProfile(accessToken) {
--- backend/src/routers/subs.js
+++ backend/src/routers/subs.js
@@ -1,13 +1,10 @@
 const router = require("express").Router();
 const axios = require("axios");
 const { authenticateUser } = require("../auth/auth");
-const { updateProviderAccessToken } = require("../auth/google/utils");
+const { updateProviderTokens } = require("../auth/google/utils");
 
-router.get("/subs", authenticateUser, async function (req, res) {
+router.get("/subs", authenticateUser, updateProviderTokens, async function (req, res) {
   try {
-    // update access token if is expired
-    req.user.providerAccessToken = await updateProviderAccessToken(req.user.email, req.user.providerRefreshToken);
-
     // make the request
     const response = await axios({
       method: "GET",
@@ -21,7 +18,6 @@ router.get("/subs", authenticateUser, async function (req, res) {
   } catch (error) {
     console.log("error.message: ", error.message);
     console.log("error.response.data: ", error.response.data);
-    //   console.log(error.response.data.error.errors[0]);
     //if (error.response.data.error) error.response.data.error.errors.forEach((e) => console.log(e));
     res.send({ error: error.message });
   }
